1662208dceb565b8420322d3f57e33260862a6cc
Aleksei Beliaev <alexey.belyaev@clevercraft.net>
2020-09-29 01:05:57 +0300

    hw2

---
+++ c#/SecondHomeWork/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        // Беляев
+        // Написать метод подсчета количества цифр числа.
+        static int getNumberDigits(int number)
+        {
+            int count = 0, copy = number;
+            while (copy > 0)
+            {
+                copy = copy / 10;
+                count++;
+                Console.WriteLine(copy);
+            }
+
+            return count;
+        }
+
+        static int getNumberDigitsRecursion(int number)
+        {
+            if (number < 1) return 0;
+            return 1 + getNumberDigitsRecursion(number / 10);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getNumberDigits(10));
+            Console.WriteLine(getNumberDigits(1));
+            Console.WriteLine(getNumberDigits(123));
+            Console.Read();
+        }
+    }
+}
