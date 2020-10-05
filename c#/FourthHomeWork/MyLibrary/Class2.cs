1b8adfb5b7e3a0d14cb31be823347786bc768b9e
Aleksei Beliaev <alexey.belyaev@clevercraft.net>
2020-10-06 01:40:01 +0300

    hw4

---
+++ c#/FourthHomeWork/MyLibrary/Class2.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary
+{
+    public class MyTwoDimensionalArray
+    {
+        int[][] a;
+
+        public MyTwoDimensionalArray(int n, int m)
+        {
+            a = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = new int[m];
+            }
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    a[i][j] = rnd.Next(1, 12);
+                    Console.WriteLine(a[i][j]);
+                }
+            }
+        }
+
+        public int Max
+        {
+            get
+            {
+                int max = a[0][0];
+                foreach (int[] row in a)
+                {
+                    foreach (int number in row)
+                    {
+                        if (number > max) max = number;
+                    }
+                }
+                return max;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                int min = a[0][0];
+                foreach (int[] row in a)
+                {
+                    foreach (int number in row)
+                    {
+                        if (number < min) min = number;
+                    }
+                }
+                return min;
+            }
+        }
+        public int Sum()
+        {
+            int sum = 0;
+            foreach (int[] row in a)
+            {
+                foreach (int number in row)
+                {
+                    sum = sum + number;
+                }
+            }
+
+            return sum;
+        }
+
+        public ref int GetMaxValueRef()
+        {
+            int max = a[0][0];
+            ref int reference = ref a[0][0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a[i].Length; j++)
+                {
+                    if (a[i][j] > max)
+                    {
+                        max = a[i][j];
+                        reference = ref a[i][j];
+                    }
+                }
+            }
+
+            return ref reference;
+        }
+        public int SumMoreThan(int value)
+        {
+            int sum = 0;
+            foreach (int[] row in a)
+            {
+                foreach (int number in row)
+                {
+                    if (number > value)
+                    {
+                        sum = sum + number;
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
