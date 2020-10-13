12d99273957ce8a5ab7ab394af5f7f0aeff61042
Aleksei Beliaev <simplelexx@gmail.com>
2020-10-14 00:04:15 +0300

    Hm5 (#5)

---
+++ c#/FifthHomeWork/Task2/Message.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class Message
+    {
+        string message;
+
+        public Message(string message)
+        {
+            this.message = message;
+        }
+        public string printMessageWithLengthLess(int countSymbols)
+        {
+            string[] words = message.Split(' ');
+            string output = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length <= countSymbols)
+                {
+                    output = output + " " + words[i];
+                }
+            }
+
+            return output;
+        }
+
+        public void removeWordsWithEnd(char symb)
+        {
+            string[] words = message.Split(' ');
+            string output = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].EndsWith(symb))
+                {
+                    continue;
+                }
+
+                output = output + " " + words[i];
+            }
+
+            message = output;
+        }
+
+        public string getMaxWord()
+        {
+            string[] words = message.Split(' ');
+            string output = "";
+
+            if (words.Length > 0)
+            {
+                output = words[0];
+
+                for (int i = 1; i < words.Length; i++)
+                {
+                    if (words[i].Length > output.Length)
+                    {
+                        output = words[i];
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        public string getMaxWords()
+        {
+            string output = "";
+            string maxWord = getMaxWord();
+            StringBuilder sbmessage = new StringBuilder(message);
+            StringBuilder word = new StringBuilder();
+
+            for (int i = 0; i < sbmessage.Length; ++i)
+            {
+                if (sbmessage[i] == ' ')
+                {
+                    if (word.Equals(maxWord))
+                    {
+                        output = output + " " + word.ToString();
+                    }
+                    word.Clear();
+                    continue;
+                }
+
+                word.Append(sbmessage[i]);
+            }
+
+            return output;
+        }
+
+        public override string ToString()
+        {
+            return message;
+        }
+    }
+}
---
+++ c#/FifthHomeWork/Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace Task3
+{
+    // Беляев
+    // *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+    // Например:
+    // badc являются перестановкой abcd.
+    class Program
+    {
+        static bool IsRearrangement(string one, string two)
+        {
+            if (one.Length != two.Length)
+            {
+                return false;
+            }
+
+            string copyOne = one;
+            string copyTwo = two;
+
+            for (int i = 0; i < copyOne.Length; i++)
+            {
+                int index = copyTwo.IndexOf(copyOne[i]);
+                if (index != -1)
+                {
+                    copyTwo = copyTwo.Remove(index, 1);
+                    continue;
+                }
+
+                return false;
+            }
+
+            return true;
+
+        }
+        static void Main(string[] args)
+        {
+            string a = "badc";
+            string b = "abcd";
+
+            Console.WriteLine(IsRearrangement(a, b));
+        }
+    }
+}
